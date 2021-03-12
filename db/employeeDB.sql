656b940ac978bbea5c298b41c567f424f4ce56ed
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-12 17:46:47 -0500

    got db to load, got main menu

--- db/employeeDB.sql
+++ db/employeeDB.sql
@@ -6,37 +6,37 @@ USE employeeDB;
 CREATE TABLE department (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL               
-)
+);
 
 CREATE TABLE role (
-    department_id INT NOT NULL UNSIGNED,
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
     Index dep_id (department_id),
-    title VARCHAR(30),
-    salary INT NOT NULL UNSIGNED
-    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES KEY role(id) ON DELETE CASCADE, 
-)
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
 CREATE TABLE employee (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL UNSIGNED,
+    role_id INT UNSIGNED NOT NULL,
     Index role_id (role_id), 
-    --foreign key
-    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE CASCADE, 
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE, 
     manager_id INT UNSIGNED,
-    Index man_id (manager_id)
-    CONSTRAINT fk_role FOREIGN KEY(manager_id) REFERENCES e,ployee(id) ON DELETE CASCADE, 
-)
+    Index man_id (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE 
+);
 
 INSERT INTO department (name)
 VALUES ('sales');
 
-INSERT INTO role (title, salary)
-VALUES ('Manager', '100,000');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Manager', '100,000', 1);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ('David', 'Eldridge')
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('David', 'Eldridge', 1, 1);
 -- constraint foreign key references
  -- manager id references role_id
     
--- index.js
+++ index.js
@@ -10,3 +10,68 @@
     //value will be something descrptive
     //create a switch case function that goes with all the choices
     //then runs a function accordingly
+
+    const inquirer = require("inquirer");
+    const mysql = require("mysql");
+   
+    const connection = mysql.createConnection({
+      host: "localhost",
+      port: process.env.PORT || 3306,
+      user: 'root',
+      password: 'Gkdve123!',
+      database: 'employeeDB'
+    });
+
+    connection.connect((err) => {
+      if (err) throw err;
+      runQuery();
+    });
+
+    const runQuery = () => {
+      console.log("Running query...");
+
+      inquirer.prompt({
+        name: "menu",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add an Employee",
+          "Remove an Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+        ],
+      })
+      .then((answer) => {
+          switch (answer.menu) {
+            case "View All Employees":
+                showAllEmp();
+                break;
+            case "View All Employees By Department":
+            case "View All Employees By Manager":
+            case "Add an Employee":
+            case "Remove an Employee":
+            case "Update Employee Role":
+            case "Update Employee Manager":
+          }
+      })
+    };
+
+const showAllEmp = () => {
+    connection.query(
+        'SELECT * FROM employee',
+        (err, res) => {
+            if (err) throw err;
+            else {
+                console.log(
+                    `Employees: ${res.employee}`
+                );
+                runQuery();
+            }
+        }
+    )
+}
+
+    module.exports = connection;
