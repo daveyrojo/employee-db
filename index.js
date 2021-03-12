7ca89813a078a0187dbe102616e2a899b040515e
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-12 17:58:43 -0500

    added employee search function

--- index.js
+++ index.js
@@ -6,72 +6,72 @@
 // View departments, roles, employees
 // Update employee roles
 //on load get a list of choices
-    //in the prompt of choices need a name of what to do
-    //value will be something descrptive
-    //create a switch case function that goes with all the choices
-    //then runs a function accordingly
+//in the prompt of choices need a name of what to do
+//value will be something descrptive
+//create a switch case function that goes with all the choices
+//then runs a function accordingly
 
-    const inquirer = require("inquirer");
-    const mysql = require("mysql");
-   
-    const connection = mysql.createConnection({
-      host: "localhost",
-      port: process.env.PORT || 3306,
-      user: 'root',
-      password: 'Gkdve123!',
-      database: 'employeeDB'
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: process.env.PORT || 3306,
+  user: "root",
+  password: "Gkdve123!",
+  database: "employeeDB",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  runQuery();
+});
+
+const runQuery = () => {
+  console.log("Running query...");
+
+  inquirer
+    .prompt({
+      name: "menu",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add an Employee",
+        "Remove an Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.menu) {
+        case "View All Employees":
+          showAllEmp();
+          break;
+        case "View All Employees By Department":
+        case "View All Employees By Manager":
+        case "Add an Employee":
+        case "Remove an Employee":
+        case "Update Employee Role":
+        case "Update Employee Manager":
+      }
     });
-
-    connection.connect((err) => {
-      if (err) throw err;
-      runQuery();
-    });
-
-    const runQuery = () => {
-      console.log("Running query...");
-
-      inquirer.prompt({
-        name: "menu",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "View All Employees",
-          "View All Employees By Department",
-          "View All Employees By Manager",
-          "Add an Employee",
-          "Remove an Employee",
-          "Update Employee Role",
-          "Update Employee Manager",
-        ],
-      })
-      .then((answer) => {
-          switch (answer.menu) {
-            case "View All Employees":
-                showAllEmp();
-                break;
-            case "View All Employees By Department":
-            case "View All Employees By Manager":
-            case "Add an Employee":
-            case "Remove an Employee":
-            case "Update Employee Role":
-            case "Update Employee Manager":
-          }
-      })
-    };
+};
 
 const showAllEmp = () => {
-    connection.query(
-        'SELECT * FROM employee',
-        (err, res) => {
-            if (err) throw err;
-            else {
-                console.log(
-                    `Employees: ${res.employee}`
-                );
-                runQuery();
-            }
-        }
-    )
-}
+  const query = 
+    "SELECT * FROM employee";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    res.forEach(({ id, first_name, last_name, role_id, manager_id }) =>
+      console.log(
+        `ID: ${id} || First Name: ${first_name} || Last Name: ${last_name} || Role ID: ${role_id} || Manager ID: ${manager_id}`
+      )
+    );
+    runQuery();
+  });
+};
 
-    module.exports = connection;
+module.exports = connection;
