aac472c0cddc468b3a578d88d6b9d08c9842dfc5
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-13 16:46:08 -0500

    function rearranging

--- index.js
+++ index.js
@@ -11,6 +11,10 @@
 //create a switch case function that goes with all the choices
 //then runs a function accordingly
 
+//questions for Dominique:
+//auto increment doesn't seem to actually work - i have to assign values
+//how do you have lists show and not disappear super fast
+
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
@@ -51,8 +55,12 @@ const runQuery = () => {
           showAllEmp();
           break;
         case "View All Employees By Department":
+          showDepart();
+          break;
         case "View All Employees By Manager":
         case "Add an Employee":
+          addEmp();
+          break;
         case "Remove an Employee":
         case "Update Employee Role":
         case "Update Employee Manager":
@@ -61,17 +69,125 @@ const runQuery = () => {
 };
 
 const showAllEmp = () => {
-  const query = 
-    "SELECT * FROM employee";
+  const query = `SELECT * FROM employee`;
   connection.query(query, (err, res) => {
-    if (err) throw err;
-    res.forEach(({ id, first_name, last_name, role_id, manager_id }) =>
-      console.log(
-        `ID: ${id} || First Name: ${first_name} || Last Name: ${last_name} || Role ID: ${role_id} || Manager ID: ${manager_id}`
-      )
-    );
-    runQuery();
+    if (err) {
+      throw err;
+    } else {
+      res.forEach(({ id, first_name, last_name, role_id, manager_id }) =>
+        console.log(
+          `ID: ${id} || First Name: ${first_name} || Last Name: ${last_name} || Role ID: ${role_id} || Manager ID: ${manager_id}`
+        )
+      );
+    }
   });
 };
+
+const showDepart = () => {
+  inquirer
+    .prompt({
+      name: "depList",
+      type: "list",
+      message: "What department would you like to choose?",
+      choices: [{ name: "Sales", value: "1" }], //"Human Resources", "Development", "Accounting"]
+    }) //need to find out how to query the search
+    .then((answer) => {
+        console.log(`---------------`);
+        console.log(`${answer.depList} `);
+        console.log(`---------------`);
+      const query = `SELECT 1 FROM employee`; //not the right statement
+      
+      connection.query(query, (err, res) => {
+        if (err) {
+          throw err;
+        } else {
+            res.forEach(({ id, first_name, last_name, role_id, manager_id }) =>
+              console.log(
+                `ID: ${id} || First Name: ${first_name} || Last Name: ${last_name} || Role ID: ${role_id} || Manager ID: ${manager_id}`
+              )
+            );
+        }
+      });
+    });
+};
+
+//do i need to use .thens to insert multiple tables or can I do it all at once?
+const addEmp = () => {
+    inquirer.prompt(
+      {
+        name: "addDep",
+        type: "list",
+        message: "What department is the employee in?",
+        choices: ["Sales", "Huma Resources", "Development"],
+      },
+      {
+        name: "title",
+        type: "list",
+        message: "What position do they have?",
+        choices: ["Manager", "Senior Level", "Junior Level"],
+      },
+      {
+        name: "salary",
+        type: "list",
+        message: "What is their salary?",
+        choices: ["100,000", "80,000", "60,000"],
+      },
+      {
+        name: "first",
+        type: "input",
+        message: "What is their first name?",
+      },
+      {
+        name: "last",
+        type: "input",
+        message: "What is their last name?",
+      },
+      {
+        name: "roleID",
+        type: "input",
+        message: "What is their role id",
+        validate: (answer) => {
+          if (isNaN(answer)) {
+            return "Please enter a number.";
+          } else {
+            return;
+          }
+        },
+      },
+      {
+        name: "manID",
+        type: "input",
+        message: "If they're a manager what is there manager id?",
+        validate: (answer) => {
+            if (answer != 'Manager') {
+                return 'This employee is not a manager.'
+            } else {
+                return;
+            }
+        }
+      }
+    ).then((answer) => {
+         connection.query(`INSERT INTO department (name) VALUES (${answer.addDep})`, (err) => {
+             if (err) throw err;
+         }).then((answer) => {
+             connection.query(`INSERT INTO role (title, salary, department_id) VALUES (${answer.title}, ${answer.salary})`, (err) => {
+                 if (err) throw err;
+             }).then((answer) => {
+                 connection.query(
+                     "INSERT INTO employee SET ? ",
+                     {
+                         first_name: answer.first,
+                         last_name: answer.last,
+                         role_id: answer.roleID,
+                         manager_id: answer.manID
+                     }
+                 )
+             })
+         });
+
+    });
+    
+    // ...need to finish code here - need to know how to insert
+}
 
 module.exports = connection;
