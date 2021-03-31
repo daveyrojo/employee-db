129b85fb7f04f230541030dc1c9e52fca1f075fe
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-30 20:20:14 -0400

    made viewAllEmp again

--- index.js
+++ index.js
@@ -95,6 +95,16 @@ const runQuery = () => {
   });
 };
 
+const viewAllEmp = () => {
+  connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager on manager.id = employee.manager_id INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id;;",
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      runQuery();
+    }
+  );
+};
 
 
 
